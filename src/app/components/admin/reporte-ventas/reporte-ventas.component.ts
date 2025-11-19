788d0643361849afb764a23f011677f89bf5630d
MiguelBM13 <mmicky619@gmail.com>
2025-11-19 12:10:18 -0500

    reporte-venta

---
+++ src/app/components/admin/reporte-ventas/reporte-ventas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reporte-ventas',
+  imports: [],
+  templateUrl: './reporte-ventas.component.html',
+  styleUrl: './reporte-ventas.component.css'
+})
+export class ReporteVentasComponent {
+
+}
